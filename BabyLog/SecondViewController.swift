e692d705af3bb1fc9b361fcdd551b05c8b269c60
kklldog <kklldog@qq.com>
2015-01-19 01:08:24 +0800

    Initial Commit

---
+++ BabyLog/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BabyLog
+//
+//  Created by mj.zhou on 15/1/19.
+//  Copyright (c) 2015年 mjstudio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
